ddb5b542b5282a36cbc9fbeee38714456e67c35d
Joseph Myrick <joseph.l.myrick2.mil@swf.army.mil>
2021-09-28 14:49:55 -0500

    Initial build.

---
+++ src/main/java/com/example/crudapipractice/CrudApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudapipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudapipractice/CrudApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudapipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
